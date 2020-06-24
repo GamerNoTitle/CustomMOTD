d7652eb1e205cb3c03b241f6026b248fd51f9d37
GamerNoTitle <bili33@87ouo.top>
2020-06-24 10:28:12 +0800

    Upload

---
+++ CustomMOTD.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+help='''=======§6CustomMOTD§r=======
+使用§6!!motd <内容>§r即可修改MOTD
+§a可以使用&颜色代码来更改颜色和格式
+§a会自动替换成§
+§d需要CarpetMOD支持！
+'''
+
+def on_info(server,info):
+    if(info.content == '!!motd'):
+        server.tell(info.player,help)
+    if ('!!motd ' in info.content):
+        motd=info.content[7:].replace('&','§')
+        server.execute('carpet setDefault customMOTD {}'.format(motd))
+        server.execute('say §e[MOTD]§r{}已经将MOTD修改为{}'.format(info.player,motd))
---
+++ README.md
@@ -0,0 +1,11 @@
+# CustomMOTD
+
+随手撸的一个修改MOTD的插件，需要CarpetMod支持
+
+游戏中使用`!!motd <内容>`即可修改MOTD，可以使用&来代替§使用颜色代码
+
+---
+
+A plugin can help you change MOTD in game, CarpetMod is required!
+
+You can use `!!motd <content>` to change the MOTD. Use & instead of § to use colorful MOTD
